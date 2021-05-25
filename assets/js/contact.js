c7437c63f21cbc9724b0a516307e8a6f57046ec8
skrrrccc <54016154+skrrrccc@users.noreply.github.com>
2021-05-25 18:37:15 +0800

    latest update

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,7 +2,6 @@ document.querySelector(".contact-form").addEventListener("submit",submitForm);
 
 function submitForm(e){
     e.preventDefault();
-
     let name = document.querySelector("#name").value;
     let email = document.querySelector("#email").value;
     let message = document.querySelector("#message").value;
@@ -15,11 +14,9 @@ function submitForm(e){
 // Send Email Info
 function sendEmail(name,email,message){
     Email.send({
-        Host:"stmp.gmail.com",
-        Username: "cknx68@gmail.com",
-        Password:"qgosxrmhqomunurc",
-        To: "cknx68@gmail.com",
-        From: "cknx68@gmail.com",
+        SecureToken :"1e97f9a3-3394-4488-a6c1-c679244ac8df",
+        To: "Auditoryandvestibular@gmail.com",
+        From: "joseph19991127@gmail.com",
         Subject: `${name} send you a message`,
         Body: `Name: ${name} <br/> Email： ${email} <br/> Message: ${message}`, 
     }).then((message) => alert("mail sent successfully"));
